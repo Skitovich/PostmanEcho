bf968e7cf978486c1f7f23a82980e9b2e265deaf
Skitovich Ruslan <zloycvetok@gmail.com>
2020-11-15 23:19:51 +0300

    Initial commit

---
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -0,0 +1,29 @@
+package ru.netology;
+
+import io.restassured.http.ContentType;
+
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    void shouldReturnDemoAccounts() {
+
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("I hope, its gonna be work")
+
+// Выполняемые действия
+                .when()
+                .post("/post")
+// Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("I hope, its gonna be work"))
+                .body("headers.content-length", equalTo("24"));
+
+    }
+}
