1bfec9676bd8593a495f9075ee88cec0220df64d
Skitovich Ruslan <zloycvetok@gmail.com>
2020-11-15 23:24:56 +0300

    test does not fall

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -23,7 +23,7 @@ public class PostmanEchoTest {
                 .statusCode(200)
                 .contentType(ContentType.JSON)
                 .body("data", equalTo("I hope, its gonna be work"))
-                .body("headers.content-length", equalTo("24"));
+                .body("headers.content-length", equalTo("25"));
 
     }
 }
